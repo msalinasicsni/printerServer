f92c7efc36fb579ed7deac1564d9dd6583a1c463
LAPTOP-J4VUVMQ9\msalinas <msalinas@icsnicaragua.org>
2022-03-01 16:34:01 -0600

    instalar servicio
    instalar servicio y crear ejecutable

---
+++ servicio/install.bat
@@ -0,0 +1,7 @@
+::Aca se indica ruta en el disco duro dónde se colocaron los archivos de la aplicación
+sc create PrinterServer binpath= C:\PrinterServer\servicio\printerServer.exe start= auto
+sc start PrinterServer
+pause
+
+
+
---
+++ servicio/uninstall.bat
@@ -0,0 +1,4 @@
+::Aca se indica ruta en el disco duro dónde se colocaron los archivos de la aplicación
+sc stop printerServer
+sc delete printerServer
+pause
